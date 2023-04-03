d146f39411aac0e915fbddb2c28f1c17a74cc085
Viniciusstsz <127238090+Viniciusstsz@users.noreply.github.com>
2023-04-03 01:21:16 -0300

    Update es704_3d.py
    Reconstrução do sinal original

--- Atividades/3/es704_3d.py
+++ Atividades/3/es704_3d.py
@@ -1,5 +1,6 @@
 import numpy as np
 import scipy.fft as fft
+import math
 import matplotlib.pyplot as plt
 
 # Carregando dados do arquivo Data03.csv
@@ -13,21 +14,30 @@ t = np.arange(N) / fs  # Vetor de tempo
 # Calculando a transformada de Fourier usando FFT
 Y = np.fft.fft(data)
 
+# Definir o número de componentes senoidais a serem usadas na reconstrução
+n_components = 100
+
 # Identificando as componentes espectrais
 freqs = np.fft.fftfreq(N, 1/fs)   # Frequências correspondentes às componentes espectrais
-idx = np.argsort(freqs)        # Ordenando as frequências
-amps = np.abs(Y[idx])          # Amplitudes das componentes espectrais
-phases = np.angle(Y[idx])      # Fases das componentes espectrais
+idx = np.argsort(freqs)              # Ordenando as frequências
+amps = 2*np.abs(Y[idx])/n_components   # Amplitudes das componentes espectrais
+phases = np.angle(Y[idx])            # Fases das componentes espectrais
 
-# Criando as ondas senoidais correspondentes às componentes espectrais
-#tiled_freqs = np.tile(freqs, (N, 1)).T   # Replicando as frequências para criar as ondas senoidais
-sin_wave = np.sum(amps * np.exp(1j * phases) * np.exp(2j * np.pi * freqs * t), axis=1)  # Criando as ondas senoidais
+# Obter as amplitudes e frequências das componentes senoidais
+#amplitudes = 2*np.abs(Y) / N
+#frequencies = np.fft.fftfreq(N, 1 / fs)
 
-# Somando as ondas senoidais criadas para obter a aproximação do sinal original
-approx_signal = np.sum(sin_wave)
+# Gerar um array vazio para armazenar a forma de onda reconstruída
+reconstructed_data = np.zeros_like(t)
 
-# Plotando o sinal original e a aproximação
-plt.plot(t, data, label='Original Signal')
-plt.plot(t, approx_signal.real, label='Approximated Signal')
+# Gerar a forma de onda reconstruída usando as amplitudes e frequências das componentes senoidais
+for i in range(n_components):
+    reconstructed_data += (amps[i][1])* np.sin(2 * math.pi * idx[i] * t + phases[i][1])
+
+# Plotar a forma de onda reconstruída
+plt.plot(t, reconstructed_data, label='Dados reconstruídos')
+
+plt.xlabel('Tempo (s)')
+plt.ylabel('Amplitude')
 plt.legend()
 plt.show()
