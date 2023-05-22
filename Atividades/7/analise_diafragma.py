5092520c38b533e48b6fed9c602f8f1f085bf024
Gabriel Morais <gabriel.hmorais11@gmail.com>
2023-05-21 21:00:51 -0300

    analise temporal

--- Atividades/7/analise_diafragma.py
+++ Atividades/7/analise_diafragma.py
@@ -1,6 +1,7 @@
 import csv
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy.signal import spectrogram
 
 # Nome do arquivo CSV
 nome_arquivo = 'Data07.csv'
@@ -44,9 +45,9 @@ pressao_media = np.mean(valores_pressao)
 pressao_rms = np.sqrt(np.mean(np.square(valores_pressao)))
 
 # Impressão das estatísticas da pressão
-print(f"Pressão Máxima: {pressao_maxima:.2e} Pa")
+print(f"\nPressão Máxima: {pressao_maxima:.2e} Pa")
 print(f"Pressão Média: {pressao_media:.2e} Pa")
-print(f"Pressão RMS: {pressao_rms:.2e} Pa")
+print(f"Pressão RMS: {pressao_rms:.2e} Pa\n")
 
 # Exibição do gráfico da pressão em uma janela separada
 plt.show()
@@ -66,7 +67,48 @@ forca_rms = np.sqrt(np.mean(np.square(valores_forca)))
 # Impressão das estatísticas da força
 print(f"Força Máxima: {forca_maxima:.2f} N")
 print(f"Força Média: {forca_media:.2f} N")
-print(f"Força RMS: {forca_rms:.2f} N")
+print(f"Força RMS: {forca_rms:.2f} N\n")
 
 # Exibição do gráfico da força em uma janela separada
+plt.show()
+
+# Cálculo da Transformada de Fourier
+fft_values = np.fft.fft(valores_pressao)
+freq = np.fft.fftfreq(len(valores_pressao), 1 / frequencia)
+
+# Gráfico da magnitude da Transformada de Fourier
+plt.figure()
+plt.plot(freq, np.abs(fft_values))
+plt.title('Espectro de Frequência')
+plt.xlabel('Frequência (Hz)')
+plt.ylabel('Magnitude')
+plt.xlim(0, frequencia / 2)  # Mostrar apenas até a metade do espectro (frequências positivas)
+plt.grid(True)
+
+# Exibição do gráfico do espectro de frequência
+plt.show()
+
+# Parâmetros da janela de análise
+window = 'hann'  # Tipo de janela (pode ser 'hann', 'hamming', 'blackman', etc.)
+window_size = 128  # Tamanho da janela
+overlap = 0.5  # Porcentagem de sobreposição entre janelas adjacentes
+
+# Cálculo do espectrograma
+frequencies, times, spectrogram_vals = spectrogram(valores_pressao, fs=frequencia, window=window,
+                                                  nperseg=window_size, noverlap=int(window_size * overlap))
+
+# Plot do espectrograma
+plt.figure()
+plt.pcolormesh(times, frequencies, 10 * np.log10(spectrogram_vals), shading='auto')
+plt.colorbar(label='Magnitude (dB)')
+plt.title('Espectrograma')
+plt.xlabel('Tempo (s)')
+plt.ylabel('Frequência (Hz)')
+
+# Define o intervalo do eixo das frequências
+freq_min = 0  # Frequência mínima desejada
+freq_max = 20 # Frequência máxima desejada
+plt.ylim(freq_min, freq_max)
+
+# Exibição do espectrograma
 plt.show()
